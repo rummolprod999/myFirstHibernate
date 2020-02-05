b5eefeed1a2dca9e9d199a53a62a0c89d165053b
rummolprod999 <rummolprod999@gmail.com>
2020-02-05 16:49:43 +0300

    add classes

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,83 @@
 package models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    private int age;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Auto> autos;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+        autos = new ArrayList<>();
+    }
+
+    public void addAuto(Auto auto) {
+        auto.setUser(this);
+        autos.add(auto);
+    }
+
+    public void removeAuto(Auto auto) {
+        autos.remove(auto);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Auto> getAutos() {
+        return autos;
+    }
+
+    public void setAutos(List<Auto> autos) {
+        this.autos = autos;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder()
+                .append("models.User{")
+                .append("id=")
+                .append(id)
+                .append(", name='")
+                .append(name)
+                .append('\'')
+                .append(", age=")
+                .append(age)
+                .append('}')
+                .toString();
+    }
 }
